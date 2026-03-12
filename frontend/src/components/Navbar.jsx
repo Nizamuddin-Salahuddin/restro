c195e15e18bb9bbb34dd6de5e991de155db2eef4
Nizamuddin <esp@NizamuddinMacBookAir.local>
2026-03-12 16:35:42 +0530

    Update navbar branding to Saffyra Kitchen

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
               <ChefHat className="w-6 h-6 text-white" />
             </div>
             <span className="text-xl font-bold">
-              Dum <span className="text-primary">&</span> Wok
+              Saffyra <span className="text-primary">Kitchen</span>
             </span>
           </Link>
 
